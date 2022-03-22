4506ccdd7d1828a0d9f8815c3ffee94103f7e7af
Rafael Moreno <rafaelmorenosax@gmail.com>
2022-03-21 22:30:01 -0300

    Aula 2

--- Aula2/app/src/main/java/com/example/aula2/MainActivity.java
+++ Aula2/app/src/main/java/com/example/aula2/MainActivity.java
@@ -8,19 +8,40 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.chip.Chip;
+
 public class MainActivity extends AppCompatActivity {
     AutoCompleteTextView autoCompleteCurso;
+    Spinner spinnerCurso;
+    Chip chip2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        autoCompleteCurso =
-                (AutoCompleteTextView) findViewById(R.id.editTextCurso);
-
+        autoCompleteCurso = (AutoCompleteTextView) findViewById(R.id.editTextCurso);
+        spinnerCurso = (Spinner)findViewById(R.id.spinnerCurso);
+        chip2 = (Chip)findViewById(R.id.chip2); 
+        
+        chip2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicou!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        chip2.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicou no fechar", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
         autoCompleteCurso.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -32,14 +53,35 @@ public class MainActivity extends AppCompatActivity {
                 return retorno;
             }
         });
-
         String[] cursos = getResources().getStringArray(R.array.cursos_array);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,cursos);
         autoCompleteCurso.setAdapter(adapter);
-
+        spinnerCurso.setAdapter(adapter);
     }
     public void buttonSalvarClick(View view){
         Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show();
+    }
+
+    public void checkBoxRepresentanteClick(View view){
+        CheckBox check = (CheckBox) view;
+        if(check.isChecked())
+            Toast.makeText(this, "É Representante", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Não é Representante", Toast.LENGTH_SHORT).show();
+    }
+
+    public void radioSexoClick(View view){
+        RadioButton radio = (RadioButton) view;
+        switch (radio.getId()){
+            case R.id.radioButtonMasculino:
+                if(radio.isChecked())
+                    Toast.makeText(this,"Masculino marcado",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radioButtonFeminino:
+                if(radio.isChecked())
+                    Toast.makeText(this,"Feminino marcado",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
