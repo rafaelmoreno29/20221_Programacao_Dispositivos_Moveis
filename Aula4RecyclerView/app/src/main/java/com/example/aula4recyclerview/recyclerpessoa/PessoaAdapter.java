28dce89f87a02d5264317888d622193a465da4aa
Rafael Moreno <rafaelmorenosax@gmail.com>
2022-03-28 20:31:32 -0300

    exemplo recycler

---
+++ Aula4RecyclerView/app/src/main/java/com/example/aula4recyclerview/recyclerpessoa/PessoaAdapter.java
@@ -0,0 +1,40 @@
+package com.example.aula4recyclerview.recyclerpessoa;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.aula4recyclerview.R;
+import com.example.aula4recyclerview.dataset.PessoaDataset;
+import com.example.aula4recyclerview.models.Pessoa;
+
+import java.util.ArrayList;
+
+public class PessoaAdapter extends RecyclerView.Adapter<PessoaHolder> {
+    private ArrayList<Pessoa> pessoas;
+
+    public PessoaAdapter(ArrayList<Pessoa> pessoas){
+        this.pessoas = pessoas;
+    }
+
+    @NonNull
+    @Override
+    public PessoaHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new PessoaHolder(
+        LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.card_item_pessoa,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PessoaHolder holder, int position) {
+        holder.nome.setText(pessoas.get(position).getNome());
+        holder.telefone.setText(pessoas.get(position).getTelefone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return pessoas != null ? pessoas.size() : 0;
+    }
+}
